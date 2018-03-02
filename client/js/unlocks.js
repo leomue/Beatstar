0c23552e2eba085661056d52847ed7f41fc52c3d
Oriol <ogomez.s92@gmail.com>
2018-03-02 09:40:50 +0100

    supermeow

---
+++ client/js/unlocks.js
@@ -0,0 +1,14 @@
+'use strict';
+import fs from 'fs';
+import os from 'os'
+import {pack,packdir} from './main';
+import {ScrollingText} from './scrollingText';
+class Settings {
+constructor() {
+this.player={
+"_CASH":0,
+"default":3013074,
+}
+}
+}
+export {Settings}
